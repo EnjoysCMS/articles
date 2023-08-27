444b2ff78bb4fc4f3a3ed458962fc110f78474af
enjoys <deadly.info@gmail.com>
2023-08-27 15:02:39 +0300

    rename Composer::assetsInstall to Assets::install

--- src/Assets.php
+++ src/Assets.php
@@ -2,14 +2,11 @@
 
 declare(strict_types=1);
 
-
 namespace EnjoysCMS\Articles;
 
-use Composer\Script\Event;
-
-final class Composer
+final class Assets
 {
-    public static function assetsInstall(Event $event)
+    public static function install(): void
     {
         passthru(sprintf('cd %s && yarn install', realpath(__DIR__.'/..')));
     }
